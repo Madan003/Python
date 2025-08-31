bc4a6d053a7581db5fe742a0d2ae6a6aa85d8927
Madan Kumar M <madanm.23.becs@acharya.ac.in>
2025-08-31 22:48:13 +0530

    Day 3 strings

---
+++ bio_generator.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+hobby = input("Enter your one Hobby: ")
+fvrt_food = input("Enter your one favorite food: ")
+fvrt_programming_lang = input("Enter your one favorite programming language: ")
+
+#"Hi, I’m Devru. I love playing Volleyball, I enjoy eating Pizza, and I’m learning Python 🚀"
+
+print("'Hi', I'm "+name+". I love playing "+hobby+", I enjoy eating "+fvrt_food+", and I'm learning "+fvrt_programming_lang+".")
+
+print(f"'Hi', I,m {name}. I love playing {hobby}, I enjoy eating {fvrt_food}, and I,m learning {fvrt_programming_lang}.")
