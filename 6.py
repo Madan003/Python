ba1dfc0c6b249993bd0dba7fba9acbd6caf65ec1
Madan Kumar M <madanm.23.becs@acharya.ac.in>
2025-09-09 01:53:49 +0530

    dictionary

---
+++ 6.py
@@ -0,0 +1,19 @@
+# dictionaries
+famous_foods = {
+    "shivamogga": "biriyani",
+    "udupi": "fish"
+}
+
+famous_foods2 = {
+    "bangalore": "dose",
+    "mysuru": "palav",
+    (1): "lists"
+}
+
+print(famous_foods)
+print(famous_foods2)
+
+#updating
+famous_foods.update(famous_foods2)
+
+print(famous_foods)
