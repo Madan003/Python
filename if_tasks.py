805fa2804f22a7078f271e43e36deadc3220cb78
Madan Kumar M <madanm.23.becs@acharya.ac.in>
2025-09-11 01:42:43 +0530

    if tasks

---
+++ if_tasks.py
@@ -0,0 +1,9 @@
+#task 1
+age = int(input("Enter your age: "))
+
+if age<5:
+    print("Bus pass is free.")
+elif age>=60:
+    print("Bus pass at discounted price.")
+else:
+    print("Pay the full amount.")
